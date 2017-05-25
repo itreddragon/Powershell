01a6abd6dfe354556f4b522c7fbcd43f88757ccc
itreddragon <itreddragon@outlook.com>
2017-05-25 15:20:38 -0400

    Add files via upload

---
+++ Copy-VMFolder/Copy-VMFolder.ps1
@@ -0,0 +1,24 @@
+﻿$paramHash = @{
+    Name = 'NA-WEB1'
+    SourcePath = 'C:\Training\FourthStreetWebsite'
+    DestinationPath = 'C:\Bakery'
+    CreateFullPath = $True
+    FileSource = 'Host'
+    Force = $True
+    Verbose = $True
+}
+$SourceBasePath = 'C:\Training\FourthStreetWebsite'
+$DestinationBasePath = 'C:\Bakery'
+
+$Files = Get-ChildItem -Path "C:\Training\FourthStreetWebsite" -Attributes !Directory -Recurse
+
+Foreach ($File in $Files) {
+    $Destination = Join-path -Path $paramHash.DestinationPath -ChildPath $File.FullName.Substring($paramHash.SourcePath.Length)
+    Write-Host "copying $($File.fullname) to $Destination" -Foreground Yellow
+    $paramHash.SourcePath = $File.fullname
+    $paramHash.DestinationPath = $Destination
+
+    Copy-VMFile @paramHash
+    $paramHash.SourcePath = $SourceBasePath
+    $paramHash.DestinationPath = $DestinationBasePath
+}
